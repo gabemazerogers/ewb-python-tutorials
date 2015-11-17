4c477c0e8ce27b614d72020e6db2b0c4de1e48ed
Gabe Maze-Rogers <gmazerog@ucsd.edu>
2015-11-16 16:51:24 -0800

    Cleaned up student.py and added better expected functionality

--- student.py
+++ student.py
@@ -1,24 +1,34 @@
 class Student(object):
-    def __init__(self, name, major, GPA, courses):
+    gpa = None
+    courses = None
+    def __init__(self, name, major):
         self.name = name
         self.major = major
-        self.gpa = 0
-        self.courses = courses
 
-    def enroll(self, course):
-        self.courses.append(course)
+    def enroll(self, *courses):
+        if(self.courses == None):
+            self.courses = []
+        for course in courses:
+            self.courses.append(course)
+        print "My courses are %s" % ', '.join(self.courses)
 
-    def drop(self, course):
-        self.courses.remove(course)
-          
+    def drop(self, *courses):
+        if(self.courses == None):
+            self.courses = []
+        for course in courses:
+            self.courses.remove(course)
+
     def introduction(self):
-        print "Hello! My name is %s, and I'm a %s major. My GPA is %s." % (self.name, self.major, self.gpa)
+        print "My name is %s, and I'm a %s major. My GPA is %s." % (self.name, self.major, "nothing because I haven't taken any classes yet" if(self.gpa == None) else self.gpa)
 
     def print_courses(self):
+        if(self.courses == None):
+            print "I'm not taking any courses."
+            return
         course_string = ""
         for course in self.courses:
             course_string += course + ','
-        print "Hello! I am taking " + course_string[:len(course_string)-1] + "!"
+        print "I am taking " + ', '.join(self.courses) + "!"
 
     def calculate_GPA(self, grades):
         grades_dict = {"A" : 4, "B" : 3, "C" : 2, "D" : 1, "F" : 0}
@@ -27,3 +37,10 @@ class Student(object):
             grade = grade[:1]
             sum += grades_dict[grade]
         self.gpa = sum / float(len(grades))
+        print "My GPA is %s!" % self.gpa
+
+gabe = Student("Gabe", "CS")
+gabe.introduction()
+gabe.calculate_GPA(['A','B','C'])
+gabe.enroll("English", "Math")
+gabe.print_courses()
